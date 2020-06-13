30ac2fdd55f56a7202e88c5c87c59263f97e26c3
Adarsh Singh <60255210+sigmoidsolutions@users.noreply.github.com>
2020-06-13 08:03:10 -0700

    Getting Bootstrap ready

--- dronasite/home/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-
-class searchForm(forms.Form):
-    searchkey = forms.CharField(max_length=100)
--- dronasite/home/views.py
+++ dronasite/home/views.py
@@ -1,19 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
-from .forms import searchForm
+from .models import streams
 # Create your views here.
 
 
 def home(request):
-    submitbutton= request.POST.get("submit")
-
-    getKey=''
-
-    form= searchForm(request.POST or None)
-    if form.is_valid():
-        getKey= form.cleaned_data.get("searchkey")
-
-
-    context= {'form': form, 'searchkey': getKey,}
-
-    return render(request, "index.html", context)
+    stream = streams()
+    return render(request, "index.html", {'stream': streams})
