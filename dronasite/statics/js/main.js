d1e8f4d76662e7c168b375d9e98b950403538ec7
Adarsh Singh <60255210+sigmoidsolutions@users.noreply.github.com>
2020-06-16 14:32:10 +0530

    card width fixed

--- dronasite/statics/js/main.js
+++ dronasite/statics/js/main.js
@@ -1,3 +1,14 @@
-function msg(){
-    alert("Hello");
+function myfunction(text){
+    alert(text + " Selected");
 }
+$(document).ready(function(){
+    $(window).scroll(function(){
+        var positiontop = $(document).scrollTop();
+        console.log(Math.ceil(positiontop))
+
+        if((positiontop > 10) && (positiontop < 213)){
+            $('#stream').addClass('animated bounceInLeft');
+        }
+        
+    });
+});
