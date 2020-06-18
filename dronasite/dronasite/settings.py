2cc3efcb049e20a3d9c708f226c33d5bdf5c2baa
Adarsh Singh <60255210+sigmoidsolutions@users.noreply.github.com>
2020-06-18 19:23:56 +0530

    Setting up Postgresql
    Using Postgresql database

--- dronasite/dronasite/settings.py
+++ dronasite/dronasite/settings.py
@@ -86,8 +86,11 @@ WSGI_APPLICATION = 'dronasite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'drona',
+        'HOST': 'localhost',
+        'USER': 'postgres',
+        'PASSWORD': '58077724'
     }
 }
 
